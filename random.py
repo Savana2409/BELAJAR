5e4449ccdf20a265703edf4f3f0da180da99bffe
Savana2409 <93864525+Savana2409@users.noreply.github.com>
2021-11-13 18:47:43 +0700

    Add files via upload

---
+++ random.py
@@ -0,0 +1,7 @@
+nama = input("masukkan nama   :  ")
+hewan = input("masukkan hewan :  ")
+sifat = input("masukkan sifat :  ")
+
+print(nama + " sangat " + "gemas")
+print(hewan + " itu " + "gendut ")
+print("sapanda " + "dan " + "panda " + 'sangat ' + sifat)
