c8efc582acfa1e7f4ba64e72c50e1b04f8dbc039
Savana2409 <93864525+Savana2409@users.noreply.github.com>
2021-11-13 19:01:56 +0700

    Add files via upload

---
+++ a.py
@@ -0,0 +1,12 @@
+namalengkap = 'Aliva Savana Septi Widianti'
+print(namalengkap)
+namapanggilan = 'Savana'
+print(namapanggilan)
+nim=212410102058
+print(nim)
+umur= '19 tahun'
+print(umur)
+tinggibadan = '163 cm'
+print(tinggibadan)
+beratbadan = '60 kg'
+print(beratbadan)
