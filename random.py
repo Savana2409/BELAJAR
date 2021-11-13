3c4f31a2ea62d6bb871f4e2722a072a192bec30c
Savana2409 <93864525+Savana2409@users.noreply.github.com>
2021-11-13 18:55:10 +0700

    Delete random.py

--- random.py
+++
@@ -1,7 +0,0 @@
-nama = input("masukkan nama   :  ")
-hewan = input("masukkan hewan :  ")
-sifat = input("masukkan sifat :  ")
-
-print(nama + " sangat " + "gemas")
-print(hewan + " itu " + "gendut ")
-print("sapanda " + "dan " + "panda " + 'sangat ' + sifat)
